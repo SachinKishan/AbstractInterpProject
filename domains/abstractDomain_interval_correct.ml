8cffa2d37f4074bb864a87fd73fc97585fed2797
Sachin Kishan <33657481+SachinKishan@users.noreply.github.com>
2026-03-20 11:50:20 -0400

    add arithmetic evaluation to interval domain

--- domains/abstractDomain_interval_correct.ml
+++ domains/abstractDomain_interval_correct.ml
@@ -19,12 +19,15 @@ type interval =
     | Bottom
     | Interval of bound * bound
 
+let top_interval = Interval(Neg_infinity, Pos_infinity)
+
 let bound_numerical_order b= 
     match b with
         | Neg_infinity -> -1000000
         | Int i  -> i
         | Pos_infinity -> 10000000
 
+(* this is made for map/set operations, not lattice *)
 let compare_bounds b1 b2=
     match (b1,b2) with
         | (Neg_infinity,Neg_infinity) -> 0
@@ -33,46 +36,119 @@ let compare_bounds b1 b2=
         | (_,Pos_infinity) -> 1
         | (Int x,Int y) -> compare x y
 
+(* meant for lattice operations, is a property used in AbstractProperty *)
+let leq_bounds b1 b2=
+    match (b1,b2) with
+        | (Neg_infinity,Neg_infinity) -> true
+        | (Pos_infinity,Pos_infinity) -> true
+        | (Pos_infinity, _) -> false
+        | (Neg_infinity,_) -> true
+        | (_,Pos_infinity) -> true
+        | (Int x,Int y) -> x <= y
+
+let min_bound b1 b2 = 
+    match leq_bounds b1 b2 with
+        | true -> b1
+        | false ->b2
+
+
+let max_bound b1 b2 = 
+    match leq_bounds b1 b2 with
+        | false -> b1
+        | true ->b2 
+
+let leq_interval i1 i2= 
+        match (i1,i2) with
+        |   (Bottom,Bottom) -> true
+        |   (Bottom,_) -> true
+        |   (_,Bottom) -> false
+        |   (Interval(b1,b2) , Interval(b3,b4)) -> leq_bounds b3 b1 && leq_bounds b2 b4
+        
+let join_interval i1 i2 = 
+        match (i1,i2) with
+            | ( Interval (a1,b1) , Interval(a2,b2) ) -> 
+                Interval(min_bound a1 a2 , max_bound b1 b2)
+            | (i1,Bottom) -> i1
+            | (Bottom,i2) -> i2     
+            
+let add_bound b1 b2 = 
+    match (b1,b2) with 
+        | (Neg_infinity,_) -> Neg_infinity
+        | (_,Neg_infinity) -> Neg_infinity
+        | (Pos_infinity,_) -> Pos_infinity
+        | (_,Pos_infinity) -> Pos_infinity
+        | (Int i, Int j) -> Int(i+j)
+
+let sub_bound b1 b2 =
+    match (b1,b2) with
+        | (Neg_infinity, Pos_infinity) -> Neg_infinity
+        | (Pos_infinity, Neg_infinity) -> Pos_infinity
+        | (Neg_infinity, Neg_infinity) -> failwith "undefined: -infinity - -infinity"
+        | (Pos_infinity, Pos_infinity) -> failwith "undefined: +infinity - +infinity"
+        | (Neg_infinity, Int j) -> Neg_infinity
+        | (Pos_infinity, Int j) -> Pos_infinity
+        | (Int i, Neg_infinity) -> Pos_infinity
+        | (Int i, Pos_infinity) -> Neg_infinity
+        | (Int i, Int j) -> Int(i-j)
+
+
 module AbstractProperty = struct
    type t = interval VariableMap.t
-   let leq = SetOfEnvironments.subset
-   let bot = SetOfEnvironments.empty
-   let join = SetOfEnvironments.union
+
+    (* env operations *)
+
+    let join m1 m2= VariableMap.merge 
+    (fun _ i1 i2 ->
+        match (i1, i2) with
+            | (Some i1, Some i2) -> Some (join_interval i1 i2)
+            | (Some i1, None)    -> Some i1          
+            | (None, Some i2)    -> Some i2          
+            | (None, None)       -> None             
+    ) m1 m2
+
+   let leq m1 m2= VariableMap.for_all (fun x i1 -> 
+        let i2 = VariableMap.find x m2 in
+            leq_interval i1 i2
+    ) m1
+   
+   let bot = VariableMap.empty
+   
+
    let initialP l = let rec of_list lt = match lt with
                     | [] -> VariableMap.empty
                     | x::lt' -> VariableMap.add x (Interval(Neg_infinity,Pos_infinity)) (of_list lt')
-                 in SetOfEnvironments.singleton (of_list l)
-   
-   let interval_plus b1 b2 = 
-   match (b1,b2) with 
+                 in of_list l
 
-   let sign_minus s1 s2 = 
-   match (s1,s2) with
-   | (Bottom , _) -> Bottom
-   | (_ ,Bottom) -> Bottom
-   | (_ ,Top) -> Top
-   | (Top,_) -> Top
-   | (x,Zero) -> x
-   | (Zero,Pos) -> Neg
-   | (Zero,Neg) -> Pos
-   | (Pos,Pos) -> Top
-   | (Pos,Neg) -> Pos
-   | (Neg, Pos) -> Neg
-   | (Neg, Neg) -> Top
+   (* interval arithmetic *)
+
+   let add_interval i1 i2=    
+       match (i1,i2) with
+            | (_,Bottom) -> Bottom
+            | (Bottom, _ ) -> Bottom
+            | (Interval(a1,b1),Interval(a2,b2)) -> Interval(add_bound a1 a2,add_bound b1 b2)
+
+
+    let sub_interval i1 i2 = 
+        match (i1,i2) with
+            | (_,Bottom) -> Bottom
+            | (Bottom, _ ) -> Bottom
+            | (Interval(a1,b1),Interval(a2,b2)) -> Interval(sub_bound a1 b2,sub_bound b1 a2)
 
    (* returns a bound given a integer after we evaluate an arithmetic expression *)
    let rec evala a r = match a with
-    | Num i -> interval(i,i)
+    | Num i -> Interval(Int i,Int i)
     | Var x -> VariableMap.find (Variable.varofstring x) r
-    | Minus (a1,a2) -> sign_minus (evala a1 r) (evala a2 r)
-    | Plus (a1,a2) -> sign_plus (evala a1 r) (evala a2 r)
+    | Minus (a1,a2) -> sub_interval (evala a1 r) (evala a2 r)
+    | Plus (a1,a2) -> add_interval (evala a1 r) (evala a2 r)
 
    let assign x a p =
       let assignenvironment r = 
          (let assignvalue y v = if (y=x) then (evala a r) else v 
              in VariableMap.mapi assignvalue r)
-      in SetOfEnvironments.map assignenvironment p
+      in assignenvironment p
 
+   (* interval boolean *)
+   
    let sign_lt s1 s2= 
     match (s1,s2) with
     |  (Neg,Pos) -> true  
